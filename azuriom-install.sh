03cb4acb14e737b243b0b40570fc41f991c91d1c
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2021-08-21 08:36:47 -0400

    MariaDB now support Debian Bullseye

--- azuriom-install.sh
+++ azuriom-install.sh
@@ -288,17 +288,10 @@ function aptinstall_mariadb() {
   if [[ "$OS" =~ (debian|ubuntu) ]]; then
     echo "MariaDB Installation"
     apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-    if [[ "$VERSION_ID" =~ (9|10|18.04|20.04) ]]; then
+    if [[ "$VERSION_ID" =~ (9|10|11|18.04|20.04) ]]; then
       echo "deb [arch=amd64] https://ftp.igh.cnrs.fr/pub/mariadb/repo/$database_ver/$ID $(lsb_release -sc) main" >/etc/apt/sources.list.d/mariadb.list
       apt-get update && apt-get install mariadb-server -y
       systemctl enable mariadb && systemctl start mariadb
-    fi
-    if [[ "$VERSION_ID" == "11" ]]; then
-      echo "deb [arch=amd64] https://ftp.igh.cnrs.fr/pub/mariadb/repo/$database_ver/debian buster main" >/etc/apt/sources.list.d/mariadb.list
-      apt-get update && apt-get install mariadb-server -y
-      systemctl enable mariadb && systemctl start mariadb
-    elif [[ "$OS" == "centos" ]]; then
-      echo "No Support"
     fi
   fi
 }
